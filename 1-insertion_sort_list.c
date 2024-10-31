77bb13aca0f4396795b5e7999395b28a303a8097
chepkoech77 <gracejohnson.koech@gmail.com>
2024-10-31 05:45:17 +0300

    main

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,62 +1,47 @@
 #include "sort.h"
+
 /**
- * swap_nodes - function to swap nodes
- * @list - list
- * @node1 - first node
- * @node2 - second node
- *
- * Return: void
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: pntr to head of the doubly-linked list.
+ * @n1: pntr to first node to swap.
+ * @n2: The 2nd node to swap.
  */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	listint_t *current;
-
-	if (node1 == NULL || node2 == NULL)
-		return;
-	if (node1->prev != NULL)
-		node1->prev->next = node2;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
 	else
-		*list = node2;
-	if (node2->next != NULL)
-		node2->next->prev = node1;
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
-
-	current = *list;
-	while (current != NULL)
-	{
-		printf("%d ", current->n);
-		current = current->next;
-	}
-	printf("\n");
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
 /**
- * insertion_sort_list - sorts the list
- * @list - the list
- *
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ * Description: Prints list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current;
+	listint_t *iter, *insert, *tmp;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	current = (*list)->next;
-	while (current != NULL)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		listint_t *insert;
-		
-		insert = current->prev;
-		while (insert != NULL && insert->n > current->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			swap_nodes(list, insert, current);
-			insert = current->prev;
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		current = current->next;
 	}
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,41 +1,44 @@
 #include "sort.h"
-/**
- * swap - swaps the elements
- * @a: first int
- * @b: second int
- *
- */
-void swap(int *a, int *b)
-{
-	int temp;
 
-	temp = *a;
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
 	*a = *b;
-	*b = temp;
+	*b = tmp;
 }
 
 /**
- * selection_sort - performs selectionsort
- * @array: array
- * @size: size of array
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
+ * Description: Prints the array after each swap.
  */
 void selection_sort(int *array, size_t size)
 {
+	int *min;
 	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		size_t min = i;
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
 
-		for (j = i + i; j < size; j++)
+		if ((array + i) != min)
 		{
-			if (array[j] < array[min])
-				min = j;
-		}
-		if (min != i)
-		{
-			swap(&array[min], &array[i]);
+			swap_ints(array + i, min);
 			print_array(array, size);
 		}
 	}
